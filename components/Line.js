f5ff38b6d638627ca9384e5223534db8a40ea858
Andrea Brugaletta <andreabrugaletta@yahoo.it>
2021-11-28 15:04:44 +0100

    touch list item

--- components/Line.js
+++ components/Line.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const Line = (props) => {
   return (
     <View style={styles.item}>
-      <Text>
-        {props.terminus1} - {props.terminus2}
+      <Text style={styles.lineName}>
+        {props.terminus1.sname} - {props.terminus2.sname}
       </Text>
-      <Text>direzione {props.terminus1}</Text>
-      <Text>direzione {props.terminus2}</Text>
+      <TouchableOpacity onPress={() => console.log(props.terminus1.did)}>
+        <Text style={styles.lineDirection}>
+          direzione {props.terminus1.sname}
+        </Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => console.log(props.terminus2.did)}>
+        <Text style={styles.lineDirection}>
+          direzione {props.terminus2.sname}
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
@@ -17,6 +25,12 @@ const styles = StyleSheet.create({
   item: {
     paddingLeft: 16,
   },
+  lineName: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    paddingVertical: 10,
+  },
+  lineDirection: { fontSize: 20, paddingVertical: 6 },
 })
 
 export default Line
--- components/LinesList.js
+++ components/LinesList.js
@@ -3,16 +3,14 @@ import { FlatList } from 'react-native'
 import Line from './Line'
 
 const LinesList = (props) => {
-  console.log(props.lines)
-
   return (
     <FlatList
       data={props.lines}
       renderItem={({ item }) => (
         <Line
-          key={item.terminus1.did}
-          terminus1={item.terminus1.sname}
-          terminus2={item.terminus2.sname}
+          keyExtractor={item.terminus1.did}
+          terminus1={item.terminus1}
+          terminus2={item.terminus2}
         />
       )}
     />
